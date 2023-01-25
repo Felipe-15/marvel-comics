4df96de5366eab988297336848a986d1ebdee35c
Felipe Souza <felipesouza036@gmail.com>
2023-01-24 22:13:54 -0300

    Adicionando campos a interface

--- src/interfaces/Comic.ts
+++ src/interfaces/Comic.ts
@@ -2,4 +2,15 @@ export interface Comic {
   id: number;
   title: string;
   thumbnail: { path: string; extensions: string };
+  resourceURI: string;
+  creators: {
+    avaiable: number;
+    items: [
+      {
+        resourceURI: string;
+        name: string;
+        role: string;
+      }
+    ];
+  };
 }
