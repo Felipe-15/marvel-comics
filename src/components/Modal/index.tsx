c5458bcf1644fc9f7d032c86162fb19b9c52fdbf
Felipe Souza <felipesouza036@gmail.com>
2023-01-30 11:40:24 -0300

    feat: Aplicação das telas do Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,57 +1,33 @@
-import {
-  Background,
-  Container,
-  DataContainer,
-  ImageContainer,
-  Image,
-  DetailsContainer,
-  Title,
-  Info,
-  WishButton,
-} from "./styles";
+import { useState } from "react";
+import { Background } from "./styles";
+
 import { Comic } from "../../interfaces/Comic";
 
+import ComicDetails from "../ComicDetails";
+import Order from "../Order";
+import FinishBuy from "../FinishBuy";
+
 type Props = {
-  handleBackgroundClick: ({}: Comic) => void;
+  handleCloseModal: (value: null) => void;
 };
 
-const Modal = ({
-  id,
-  title,
-  pageCount,
-  thumbnail,
-  creators,
-  series,
-  handleBackgroundClick,
-}: Comic & Props) => {
-  const creator = creators.items[0]?.name;
+const Modal = ({ handleCloseModal, ...comic }: Comic & Props) => {
+  const [showOrder, setShowOrder] = useState(false);
+  const [address, setAddress] = useState("");
 
   return (
-    <Background onClick={() => handleBackgroundClick({} as Comic)}>
-      <Container>
-        <DataContainer>
-          <ImageContainer>
-            <Image
-              src={`${
-                thumbnail.path + `/portrait_fantastic.${thumbnail.extension}`
-              }`}
-            />
-          </ImageContainer>
-          <DetailsContainer>
-            <Title>{title}</Title>
-            <Info>
-              <strong>Autor:</strong> {creator || "Autor Indisponível"}
-            </Info>
-            <Info>
-              <strong>Número de páginas:</strong> {pageCount || "Indisponível"}
-            </Info>
-            <Info>
-              <strong>Série:</strong> {series.name || "Indisponível"}
-            </Info>
-          </DetailsContainer>
-        </DataContainer>
-        <WishButton>Encomendar</WishButton>
-      </Container>
+    <Background onClick={() => handleCloseModal(null)}>
+      {!showOrder ? (
+        <ComicDetails {...comic} handleOrder={setShowOrder} />
+      ) : !address ? (
+        <Order handleCancel={handleCloseModal} handleSetLocation={setAddress} />
+      ) : (
+        <FinishBuy
+          comicName={comic.title}
+          address={address}
+          handleClose={handleCloseModal}
+        />
+      )}
     </Background>
   );
 };
